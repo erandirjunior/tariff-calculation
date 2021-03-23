0b600b01be41554e75a06919fe558f685bb9cced
Erandir Junior <aefs12junior@gmail.com>
2021-03-23 11:34:17 -0300

    Added new routes

--- routes/routes.php
+++ routes/routes.php
@@ -2,9 +2,38 @@
 
 $app = \Slim\Factory\AppFactory::createFromContainer($container);
 
-$app->get('/', function (){
-   $a = new \SRC\TariffCalculation\Infra\ExternalService\GetCurrentExchange();
-   var_dump($a->getValue('USD'));
+$app->group('/users', function (\Slim\Routing\RouteCollectorProxy $group) {
+    $group->get('/{id:[0-9]+}', \SRC\User\Infra\Http\FinderByIdentifier::class);
+
+    $group->get('', \SRC\User\Infra\Http\FindAll::class);
+
+    $group->post('', \SRC\User\Infra\Http\Register::class);
+
+    $group->delete('/{id:[0-9]+}', \SRC\User\Infra\Http\Destroy::class);
+
+    $group->put('/{id:[0-9]+}', \SRC\User\Infra\Http\Update::class);
+
+    $group->group('/{userId:[0-9]+}/bookings', function (\Slim\Routing\RouteCollectorProxy $group) {
+        $group->get('', \SRC\Booking\Infra\Http\FindAll::class);
+
+        $group->get('/{id:[0-9]+}', \SRC\Booking\Infra\Http\FinderByIdentifier::class);
+
+        $group->post('', \SRC\Booking\Infra\Http\Register::class);
+
+        $group->delete('/{id:[0-9]+}', \SRC\Booking\Infra\Http\Destroy::class);
+    });
+});
+
+$app->group('/sellers', function (\Slim\Routing\RouteCollectorProxy $group) {
+    $group->get('/{id:[0-9]+}', \SRC\Seller\Infra\Http\FinderByIdentifier::class);
+
+    $group->get('', \SRC\Seller\Infra\Http\FindAll::class);
+
+    $group->post('', \SRC\Seller\Infra\Http\Register::class);
+
+    $group->delete('/{id:[0-9]+}', \SRC\Seller\Infra\Http\Destroy::class);
+
+    $group->put('/{id:[0-9]+}', \SRC\Seller\Infra\Http\Update::class);
 });
 
 $app->group('/coins', function (\Slim\Routing\RouteCollectorProxy $group) {
